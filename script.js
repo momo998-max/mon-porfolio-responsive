fb7cbc6ab46850ed6175e32e81eb77775cd03c8c
Ibn'Mamour_Dev <62941860+momo998-max@users.noreply.github.com>
2023-04-12 13:08:42 +0000

    mon commit

---
+++ script.js
@@ -0,0 +1,36 @@
+/*menu scroll */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+};
+
+/*CHANGEMENT ACTIVE*/
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('stiky', window.scrollY > 100);
+    /*supprimer le barre de menu un fois seclectionner un dispo */
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+};
+
+
+
